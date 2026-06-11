1e2cbdb3cd09654f4cdd4a53ba33bbfe150ebe62
marcoaccordi83 <marco.accordi@gmail.com>
2026-06-11 09:08:31 +0200

    Update supabase.js
    env variables

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://syysfydwdaoztqdrqyzx.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5eXNmeWR3ZGFvenRxZHJxeXp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMjQwMzMsImV4cCI6MjA5NjYwMDAzM30.02he2fYNTOYkud_3A_EGzNqDtuJ8gBnz6YSaPrUEn5k'
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
